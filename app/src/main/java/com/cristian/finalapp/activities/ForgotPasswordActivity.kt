a2504e17bbc5191687711bd6c836b772d4cc58e5
CristianCastellanos <cristiancastellanos7@gmail.com>
2020-04-27 10:26:20 -0500

    Create class SingUpActivity and ForgotPasswordActivity.
    Create the interface for SingUpActivity, three edittext and two buttons

---
+++ app/src/main/java/com/cristian/finalapp/activities/ForgotPasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.cristian.finalapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cristian.finalapp.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
---
+++ app/src/main/java/com/cristian/finalapp/activities/SingUpActivity.kt
@@ -0,0 +1,13 @@
+package com.cristian.finalapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cristian.finalapp.R
+
+class SingUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
