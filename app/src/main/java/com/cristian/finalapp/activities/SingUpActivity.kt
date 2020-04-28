2bb49052781dcdea44a7994b1e6beb728fe0768b
CristianCastellanos <cristiancastellanos7@gmail.com>
2020-04-28 10:29:41 -0500

    Implement the code for singUpActivity, create method for button login, sign up.
    Create method for valid email, password when the text field don't null.

--- app/src/main/java/com/cristian/finalapp/activities/SingUpActivity.kt
+++ app/src/main/java/com/cristian/finalapp/activities/SingUpActivity.kt
@@ -1,13 +1,60 @@
 package com.cristian.finalapp.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.cristian.finalapp.R
+import com.cristian.finalapp.goToActivity
+import com.cristian.finalapp.toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.editTextEmail
+import kotlinx.android.synthetic.main.activity_login.editTextPassword
+import kotlinx.android.synthetic.main.activity_sing_up.*
+
 
 class SingUpActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sing_up)
+
+        buttonLogin.setOnClickListener {
+            goToActivity<LoginActivity> {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            /*val intent = Intent(this,LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)*/
+        }
+
+        buttonSignUp.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            if (isValidEmailAndPassword(email, password)) {
+                singUpByEmail(email, password)
+            } else {
+                toast("Please fill all the data and confirm password is correct.")
+            }
+        }
+    }
+
+    private fun singUpByEmail(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    toast("An email has been sent to you. Please, confirmberfore sign in")
+                    val user = mAuth.currentUser
+                } else {
+                    toast("An unexpected error occurred, please try again")
+                }
+            }
+    }
+
+    private fun isValidEmailAndPassword(email: String, password: String): Boolean {
+        return !email.isNullOrEmpty() && !password.isNullOrEmpty() &&
+                password == editTextConfirmPassword.text.toString()
     }
 }
