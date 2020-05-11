2e3be77c0511fc220347716266ffe100926ac87d
CristianCastellanos <cristiancastellanos7@gmail.com>
2020-05-11 12:04:59 -0500

    logic to login, create method for valid email and password, implement button for login and click listener for forgot password.

--- app/src/main/java/com/cristian/finalapp/activities/LoginActivity.kt
+++ app/src/main/java/com/cristian/finalapp/activities/LoginActivity.kt
@@ -3,11 +3,48 @@ package com.cristian.finalapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cristian.finalapp.R
+import com.cristian.finalapp.goToActivity
+import com.cristian.finalapp.toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonLogin.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            val password = editTextPassword.text.toString()
+            if (isValidEmailAndPassword(email, password)) {
+                logInByEmail(email, password)
+            }
+        }
+
+        textViewForgotPassword.setOnClickListener {
+            goToActivity<ForgotPasswordActivity>()
+        }
+
+        buttonCreateAccount.setOnClickListener {
+            goToActivity<SignUpActivity>()
+        }
+
+    }
+
+    private fun logInByEmail (email: String, password:String) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                toast("User is now logged in.")
+            } else {
+                toast("An unexpected error occurred, please try again.")
+            }
+        }
+    }
+
+    private fun isValidEmailAndPassword(email: String, password: String): Boolean {
+        return !email.isNullOrEmpty() && !password.isNullOrEmpty()
     }
 }
