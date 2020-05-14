95ca47f0638663d0788a590b656daa3e3c25764b
CristianCastellanos <cristiancastellanos7@gmail.com>
2020-05-14 10:37:39 -0500

    Move methods of sign up, to validate email, password to extension archive

--- app/src/main/java/com/cristian/finalapp/Extensions.kt
+++ app/src/main/java/com/cristian/finalapp/Extensions.kt
@@ -2,9 +2,14 @@ package com.cristian.finalapp
 
 import android.app.Activity
 import android.content.Intent
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
+import java.util.regex.Pattern
 
 fun Activity.toast(message: CharSequence, duration: Int = Toast.LENGTH_SHORT) = Toast.makeText(this,message,duration).show()
 
@@ -16,4 +21,34 @@ inline fun <reified  T: Activity> Activity.goToActivity(noinline  init: Intent.(
     val intent = Intent(this,T::class.java)
     intent.init()
     startActivity(intent)
+}
+
+fun EditText.validate(validation: (String) -> Unit) {
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(s: Editable) {
+            validation(s.toString())
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+    })
+}
+
+fun Activity.isValidEmail(email: String): Boolean {
+    val pattern = Patterns.EMAIL_ADDRESS
+    return pattern.matcher(email).matches()
+}
+
+fun Activity.isValidPassword(password: String): Boolean {
+    val passwordPatterns = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$"
+    val pattern = Pattern.compile(passwordPatterns)
+    return pattern.matcher(password).matches()
+}
+
+fun Activity.isValidConfirmPassword(password: String, confirmPassword: String): Boolean {
+    return password == confirmPassword
 }
