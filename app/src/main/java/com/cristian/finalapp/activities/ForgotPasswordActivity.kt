48e91487b0865de88f17ed068e78ef23a52d9702
CristianCastellanos <cristiancastellanos7@gmail.com>
2020-05-14 11:45:14 -0500

    Create logic to forgot my password, implement message for error in to edittext. Implement the logic to button forgot.

--- app/src/main/java/com/cristian/finalapp/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/cristian/finalapp/activities/ForgotPasswordActivity.kt
@@ -1,20 +1,44 @@
 package com.cristian.finalapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cristian.finalapp.R
-import com.cristian.finalapp.goToActivity
+import com.cristian.finalapp.*
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_forgot_password.*
 
 class ForgotPasswordActivity : AppCompatActivity() {
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
 
+        editTextEmail.validate {
+            editTextEmail.error = if (isValidEmail(it)) null else "Email is not valid"
+        }
+
         buttonGoLogin.setOnClickListener {
-            goToActivity<LoginActivity>()
+            goToActivity<LoginActivity>() {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        }
+
+        buttonForgot.setOnClickListener {
+            val email = editTextEmail.text.toString()
+            if (isValidEmail(email)) {
+                mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this) {
+                    toast("Email has been sent to reset your password.")
+                    goToActivity<LoginActivity>() {
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    }
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                }
+            } else {
+                toast("Please make sure the email address is correct.")
+            }
         }
     }
 }
